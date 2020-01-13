97619c3df20c4e4ac6629aa8f47640df3326caf7
Trung Nguyen <trungnguyen5@deloitte.com>
2020-01-13 14:56:26 +0700

    3 - Create Unit Test Project

--- UnitTest/Calculator/Program.cs
+++ UnitTest/Calculator/Program.cs
@@ -7,6 +7,7 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ UnitTest/Test/Services/AdditionServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Test.Services
+{
+    public class AdditionServiceTest
+    {
+        [Fact]
+        public void Add_OneAndTwo_ReturnThree()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
